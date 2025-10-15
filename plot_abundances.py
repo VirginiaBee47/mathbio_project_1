fd8579bd59111b8f745d754c9e9ffe5abc231cef
Gavin <gavinpha@outlook.com>
2025-10-15 15:52:55 -0400

    Add utility data plot scripts

---
+++ plot_abundances.py
@@ -0,0 +1,29 @@
+import csv
+import matplotlib.pyplot as plt
+
+with open('abundance_data.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    juveniles = []
+    females = []
+    males = []
+
+    first_row = True
+
+    for row in csv_reader:
+        if first_row:
+            first_row = False
+            continue
+
+        juveniles.append(float(row[6]) / 1000.0)
+        females.append(float(row[7]) / 1000.0)
+        males.append(float(row[8]) / 1000.0)
+
+    plt.plot(juveniles, label='Juveniles')
+    plt.plot(females, label='Females')
+    plt.plot(males, label='Males')
+    plt.xlabel('Time (years)')
+    plt.ylabel('Abundance (thousands)')
+    plt.title('Calculated abundances of Spotted Seatrout')
+    plt.legend()
+    plt.show()
---
+++ plot_catch.py
@@ -0,0 +1,22 @@
+import csv
+import matplotlib.pyplot as plt
+
+with open('catch_harvest_data.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    catch = []
+
+    first_row = True
+
+    for row in csv_reader:
+        if first_row:
+            first_row = False
+            continue
+
+        catch.append(float(row[0]) / 1000.0)
+
+    plt.plot(catch, label='Catch')
+    plt.xlabel('Time (years)')
+    plt.ylabel('Abundance (thousands)')
+    plt.title('Annual catch of Spotted Seatrout')
+    plt.show()
