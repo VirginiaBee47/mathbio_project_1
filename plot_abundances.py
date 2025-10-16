3db8056375ce690d7c26ff27fe333edad4dc6540
Virginia Covert <virginiacovert454@gmail.com>
2025-10-15 20:34:24 -0400

    Made plots importable/callable.

--- plot_abundances.py
+++ plot_abundances.py
@@ -1,29 +1,34 @@
 import csv
 import matplotlib.pyplot as plt
 
-with open('abundance_data.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+def plot_abundances():
+    with open('abundance_data.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
 
-    juveniles = []
-    females = []
-    males = []
+        juveniles = []
+        females = []
+        males = []
 
-    first_row = True
+        first_row = True
 
-    for row in csv_reader:
-        if first_row:
-            first_row = False
-            continue
+        for row in csv_reader:
+            if first_row:
+                first_row = False
+                continue
 
-        juveniles.append(float(row[6]) / 1000.0)
-        females.append(float(row[7]) / 1000.0)
-        males.append(float(row[8]) / 1000.0)
+            juveniles.append(float(row[6]) / 1000.0)
+            females.append(float(row[7]) / 1000.0)
+            males.append(float(row[8]) / 1000.0)
 
-    plt.plot(juveniles, label='Juveniles')
-    plt.plot(females, label='Females')
-    plt.plot(males, label='Males')
-    plt.xlabel('Time (years)')
-    plt.ylabel('Abundance (thousands)')
-    plt.title('Calculated abundances of Spotted Seatrout')
-    plt.legend()
-    plt.show()
+        plt.plot(juveniles, label='Juveniles')
+        plt.plot(females, label='Females')
+        plt.plot(males, label='Males')
+        plt.xlabel('Time (years)')
+        plt.ylabel('Abundance (thousands)')
+        plt.title('Calculated abundances of Spotted Seatrout')
+        plt.legend()
+        plt.show()
+
+if __name__ == "__main__":
+    plot_abundances()
+    
--- plot_catch.py
+++ plot_catch.py
@@ -1,22 +1,26 @@
 import csv
 import matplotlib.pyplot as plt
 
-with open('catch_harvest_data.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+def plot_catch():
+    with open('catch_harvest_data.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
 
-    catch = []
+        catch = []
 
-    first_row = True
+        first_row = True
 
-    for row in csv_reader:
-        if first_row:
-            first_row = False
-            continue
+        for row in csv_reader:
+            if first_row:
+                first_row = False
+                continue
 
-        catch.append(float(row[0]) / 1000.0)
+            catch.append(float(row[0]) / 1000.0)
 
-    plt.plot(catch, label='Catch')
-    plt.xlabel('Time (years)')
-    plt.ylabel('Abundance (thousands)')
-    plt.title('Annual catch of Spotted Seatrout')
-    plt.show()
+        plt.plot(catch, label='Catch')
+        plt.xlabel('Time (years)')
+        plt.ylabel('Abundance (thousands)')
+        plt.title('Annual catch of Spotted Seatrout')
+        plt.show()
+
+if __name__ == "__main__":
+    plot_catch()
